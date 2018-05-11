fd48415e0e39ece77c51a1e63fc3915547523a0b
sofiakulymanova <sofiakulymanova@gmail.com>
2018-05-11 14:34:10 +0400

    Домашняя 1

---
+++ HW1/HW1/Game.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1
+{
+    public class Game
+    {
+        int size;
+        int[][] field;
+
+        public Game(int s)
+        {
+            size = s;
+            field = new int[size][];
+            for (int i = 0; i < size; i++)
+            {
+                field[i] = new int[size];
+            }
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    field[i][j] = -1;
+                }
+            }
+        }
+
+        public void add(int x, int y, int num)
+        {
+            if (field[x][y] == -1)
+            {
+                field[x][y] = num;
+            }
+        }
+
+        public bool check()
+        {
+            bool g = false, v = false, dg = false,dp=false;
+            for(int i = 0; i < size-1; i++)
+            {
+                for(int j = 0; j < size-1; j++)
+                {
+                    if (field[i][j] != -1)
+                    {
+                        if (field[i][j] != field[i + 1][j])
+                        {
+                            g = false;
+                        }
+                        else g = true;
+                        if (field[i][j] != field[i][j + 1])
+                        {
+                            v = false;
+                        }
+                        else v = true;
+                    }
+                    if (field[i][i] != -1)
+                    {
+                        if (field[i][i] != field[i + 1][i + 1])
+                        {
+                            dg = false;
+                        }
+                        else dg = true;
+                    }
+                    if (field[size - 1 - i][i] != -1)
+                    {
+                        if (field[size - 1 - i][i] != field[size-2-i][i+1])
+                        {
+                            dp = false;
+                        }
+                        else dp = true;
+                    }
+                }
+            }
+            if (g == true || v == true || dg == true || dp == true) return true;
+            return false;
+        }
+
+        public string gameFild()
+        {
+            string str = "";
+            for(int i = 0; i < size; i++)
+            {
+                for(int j = 0; j < size; j++)
+                {
+                    str += field[i][j] + " ";
+                }
+                str += "\n";
+            }
+            return str;
+        }
+    }
+}
---
+++ HW1/HW1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int s = Convert.ToInt32(Console.ReadLine());
+                Game game = new Game(s);
+                while (!game.check())
+                {
+                    int x = 0, y = 0, num = 0;
+                    x = Convert.ToInt32(Console.ReadLine());
+                    y = Convert.ToInt32(Console.ReadLine());
+                    num = Convert.ToInt32(Console.ReadLine());
+                    if (x > s || x < 0 || y > s || y < 0 || (num != 0 && num != 1))
+                    {
+                        Console.WriteLine("Введенные вами данные не верны");
+                        Environment.Exit(0);
+                    }
+
+                    game.add(x, y, num);
+                    Console.WriteLine(game.gameFild());
+                }
+                Console.WriteLine("Game Over!!! \n" + game.gameFild());
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Ошибка \n" + ex);
+                Environment.Exit(0);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Ошибка \n" + ex);
+                Environment.Exit(0);
+            }
+        }
+    }
+}
